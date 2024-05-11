8e85d5a25b9ae9f3451f15dd339860504b165397
AmieeYL <yiling4140@gmail.com>
2024-05-11 15:14:38 -0400

    add: find all/single movies

--- src/main/java/com/example/movieist/MovieRepository.java
+++ src/main/java/com/example/movieist/MovieRepository.java
@@ -1,4 +1,10 @@
 package com.example.movieist;
 
-public interface MovieRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.bson.types.ObjectId;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
 }
--- src/main/java/com/example/movieist/MovieistApplication.java
+++ src/main/java/com/example/movieist/MovieistApplication.java
@@ -1,14 +1,29 @@
 package com.example.movieist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-public class MovieistApplication {
+public class MovieistApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieistApplication.class, args);
+	}
+
+	@Autowired
+	private MovieService movieService;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("here");
+		List<Movie> movies = movieService.allMovies();
+		movies.forEach(movie -> System.out.println(movie.toString()));
 	}
 }
